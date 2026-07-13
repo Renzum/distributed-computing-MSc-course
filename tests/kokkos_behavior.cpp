c1817a17fa418ef5e9d8f9f1012aedc94e9c69d5
Renzum <31014384+Renzum@users.noreply.github.com>
2026-07-13 18:13:19 +0200

    fixup! Refactor

--- tests/kokkos_behavior.cpp
+++ tests/kokkos_behavior.cpp
@@ -12,17 +12,7 @@ TEST(KOKKOS_BEHAVIOR_TEST, CONTIGIOUS_MEMORY) {
     LatticeBoltzmann::DistributionInitializers::random_density(
         distribution_function);
 
-    LatticeBoltzmann::DistributionFunction::HostMirror host_mirror =
-        Kokkos::create_mirror_view(distribution_function);
+    auto host_mirror = Kokkos::create_mirror_view(distribution_function);
 
     ASSERT_TRUE(host_mirror.span_is_contiguous());
-
-    Kokkos::deep_copy(host_mirror, distribution_function);
-
-    for (int x = 0; x < 20; x++) {
-        for (int y = 0; y < 20; y++) {
-            ASSERT_EQ(host_mirror(x, y, Direction::Center),
-                      distribution_function(x, y, Direction::Center));
-        }
-    }
 }
