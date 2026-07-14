da105052b66eaa7f8136edf4c9b73f55980a7e56
Renzum <31014384+Renzum@users.noreply.github.com>
2026-07-14 12:08:41 +0000

    fixup! Start the MPI communication implementation

--- executables/milestone06/main.cpp
+++ executables/milestone06/main.cpp
@@ -32,7 +32,7 @@ class Node {
     // Buffers for moving slices from GPU to CPU and vice versa
     using GPUBuffer = Kokkos::View<double *[TOTAL_DIRECTIONS]>;
     GPUBuffer left_right_gpu_buffer;
-    // GPUBuffer up_down_gpu_buffer;
+    GPUBuffer up_down_gpu_buffer;
 
     // CPU Space Buffers for contagious data sending via MPI
     using SendRecvBuffer = GPUBuffer::HostMirror;
@@ -40,9 +40,9 @@ class Node {
     SendRecvBuffer left_right_recv_buffer;
     int left_right_buffer_len;
 
-    // SendRecvBuffer up_down_send_buffer;
-    // SendRecvBuffer up_down_recv_buffer;
-    // int h_buffer_len;
+    SendRecvBuffer up_down_send_buffer;
+    SendRecvBuffer up_down_recv_buffer;
+    int up_down_buffer_len;
 
   public:
     int lattice_width, lattice_height;
@@ -99,22 +99,23 @@ class Node {
         lattice_height = GLOBAL_DOMAIN_SIZE / mpi_domain_size +
                          ghost_layers.down + ghost_layers.up;
 
-        left_right_gpu_buffer = Kokkos::View<double *[TOTAL_DIRECTIONS]>(
-            "Horzintal GPU Buffer", lattice_height);
+        left_right_gpu_buffer =
+            GPUBuffer("Horzintal GPU Buffer", lattice_height);
 
         left_right_send_buffer =
             Kokkos::create_mirror_view(left_right_gpu_buffer);
         left_right_recv_buffer = Kokkos::create_mirror(left_right_gpu_buffer);
         left_right_buffer_len = lattice_height * TOTAL_DIRECTIONS;
 
-        // up_down_gpu_buffer("Vertical GPU Buffer", lattice_width);
-        // up_down_send_buffer = Kokkos::create_mirror_view(up_down_gpu_buffer);
-        // up_down_recv_buffer = Kokkos::create_mirror(up_down_recv_buffer);
-        // h_buffer_len = lattice_height * TOTAL_DIRECTIONS;
+        up_down_gpu_buffer = GPUBuffer("Vertical GPU Buffer", lattice_width);
+
+        up_down_send_buffer = Kokkos::create_mirror_view(up_down_gpu_buffer);
+        up_down_recv_buffer = Kokkos::create_mirror(up_down_gpu_buffer);
+        up_down_buffer_len = lattice_width * TOTAL_DIRECTIONS;
     }
 
-    void
-    communicate(LatticeBoltzmann::DistributionFunction &distribution_function) {
+    void communicate_left(
+        LatticeBoltzmann::DistributionFunction &distribution_function) {
         MPI_Status status;
         if (left != MPI_PROC_NULL) {
             std::cout << fmt::format("{:d}: Sending left to {:d}", rank, left)
@@ -143,6 +144,112 @@ class Node {
             Kokkos::deep_copy(right_subview, left_right_gpu_buffer);
         }
     }
+
+    void communicate_right(
+        LatticeBoltzmann::DistributionFunction &distribution_function) {
+        MPI_Status status;
+        if (right != MPI_PROC_NULL) {
+            std::cout << fmt::format("{:d}: Sending right to {:d}", rank, right)
+                      << std::endl;
+            auto right_subview =
+                Kokkos::subview(distribution_function, lattice_width - 2,
+                                Kokkos::ALL, Kokkos::ALL);
+            Kokkos::deep_copy(left_right_gpu_buffer, right_subview);
+            Kokkos::deep_copy(left_right_send_buffer, left_right_gpu_buffer);
+        }
+
+        MPI_Sendrecv(left_right_send_buffer.data(), left_right_buffer_len,
+                     MPI_DOUBLE, right, 0, left_right_recv_buffer.data(),
+                     left_right_buffer_len, MPI_DOUBLE, left, 0, cart, &status);
+
+        if (left != MPI_PROC_NULL) {
+            std::cout << fmt::format("{:d}: Receiving left from {:d}", rank,
+                                     left)
+                      << std::endl;
+
+            auto left_subview = Kokkos::subview(distribution_function, 0,
+                                                Kokkos::ALL, Kokkos::ALL);
+
+            Kokkos::deep_copy(left_right_gpu_buffer, left_right_recv_buffer);
+            Kokkos::deep_copy(left_subview, left_right_gpu_buffer);
+        }
+    }
+
+    void communicate_down(
+        LatticeBoltzmann::DistributionFunction &distribution_function) {
+        MPI_Status status;
+
+        if (down != MPI_PROC_NULL) {
+            std::cout << fmt::format("{:d}: Sending down to {:d}", rank, down)
+                      << std::endl;
+            auto down_subview = Kokkos::subview(distribution_function,
+                                                Kokkos::ALL, 1, Kokkos::ALL);
+            Kokkos::deep_copy(up_down_gpu_buffer, down_subview);
+            Kokkos::deep_copy(up_down_send_buffer, left_right_gpu_buffer);
+        }
+
+        MPI_Sendrecv(up_down_send_buffer.data(), up_down_buffer_len, MPI_DOUBLE,
+                     down, 0, up_down_recv_buffer.data(), up_down_buffer_len,
+                     MPI_DOUBLE, up, 0, cart, &status);
+
+        if (up != MPI_PROC_NULL) {
+            std::cout << fmt::format("{:d}: Receiving up from {:d}", rank, up)
+                      << std::endl;
+
+            auto up_subview =
+                Kokkos::subview(distribution_function, Kokkos ::ALL,
+                                lattice_height - 1, Kokkos::ALL);
+
+            Kokkos::deep_copy(up_down_gpu_buffer, up_down_recv_buffer);
+            Kokkos::deep_copy(up_subview, left_right_gpu_buffer);
+        }
+    }
+
+    void communicate_up(
+        LatticeBoltzmann::DistributionFunction &distribution_function) {
+        MPI_Status status;
+
+        if (up != MPI_PROC_NULL) {
+            std::cout << fmt::format("{:d}: Sending up to {:d}", rank, down)
+                      << std::endl;
+            auto up_subview =
+                Kokkos::subview(distribution_function, Kokkos::ALL,
+                                lattice_height - 2, Kokkos::ALL);
+            Kokkos::deep_copy(up_down_gpu_buffer, up_subview);
+            Kokkos::deep_copy(up_down_send_buffer, left_right_gpu_buffer);
+        }
+
+        MPI_Sendrecv(up_down_send_buffer.data(), up_down_buffer_len, MPI_DOUBLE,
+                     up, 0, up_down_recv_buffer.data(), up_down_buffer_len,
+                     MPI_DOUBLE, down, 0, cart, &status);
+
+        if (down != MPI_PROC_NULL) {
+            std::cout << fmt::format("{:d}: Receiving down from {:d}", rank,
+                                     down)
+                      << std::endl;
+
+            auto down_subview = Kokkos::subview(distribution_function,
+                                                Kokkos ::ALL, 0, Kokkos::ALL);
+
+            Kokkos::deep_copy(up_down_gpu_buffer, up_down_recv_buffer);
+            Kokkos::deep_copy(down_subview, left_right_gpu_buffer);
+        }
+    }
+
+    void communicate_all(
+        LatticeBoltzmann::DistributionFunction &distribution_function) {
+        // Skip horizontal communication if no left or right neighbors exist
+        if (left != MPI_PROC_NULL || right != MPI_PROC_NULL) {
+            communicate_left(distribution_function);
+            communicate_right(distribution_function);
+        }
+
+        // Skip vertical communication if no up or down neighbors exist
+        if (down != MPI_PROC_NULL || up != MPI_PROC_NULL) {
+            communicate_down(distribution_function);
+            communicate_up(distribution_function);
+        }
+    };
 };
 
 int main(int argc, char *argv[]) {
@@ -185,7 +292,7 @@ int main(int argc, char *argv[]) {
             std::cout << "starting iterations" << std::endl;
             for (int i = 0; i < iterations; i++) {
                 Kokkos::fence();
-                node.communicate(distribution_function);
+                node.communicate_all(distribution_function);
             }
         }
     }
