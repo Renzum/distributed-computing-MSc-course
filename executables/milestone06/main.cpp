86604b1e7f8a7af7709e3a20eb065d32dbb1b804
Renzum <31014384+Renzum@users.noreply.github.com>
2026-08-23 19:32:48 +0200

    Remove printing from ms6
    Will only be used for benchmarking MPI

--- executables/milestone06/main.cpp
+++ executables/milestone06/main.cpp
@@ -1,4 +1,3 @@
-#include <filesystem>
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -13,69 +12,8 @@
 #include <mpi_layer.hpp>
 #include <output_functions.hpp>
 
-class DistributedVelocityProfileOutput {
-    std::ofstream output_file;
-
-    // Since we're writing each MPI node's velocity into a separate file, we
-    // need to keep track of the x and y offsets of the local lattice in
-    // relation to the global lattice according to the domain decomposition
-    int x_offset, y_offset;
-    int lattice_width, lattice_height;
-    MPILayer::GhostLayers ghost_layers;
-
-  public:
-    DistributedVelocityProfileOutput(std::filesystem::path &output_directory,
-                                     int domain_width, int domain_height,
-                                     MPILayer &mpi_layer) {
-
-        ghost_layers = mpi_layer.ghost_layers;
-
-        lattice_width = mpi_layer.lattice_width;
-        lattice_height = mpi_layer.lattice_height;
-
-        auto file_path = output_directory.concat(fmt::format(
-            "/velocity_profile_{:d}.data", mpi_layer.mpi_data->rank));
-
-        output_file = std::ofstream(file_path, std::ios::out | std::ios::trunc);
-
-        x_offset = mpi_layer.mpi_data->coords[1] *
-                   static_cast<int>(domain_width / mpi_layer.mpi_data->dims[1]);
-        if (mpi_layer.mpi_data->coords[1] > 0) {
-            x_offset += (domain_width % mpi_layer.mpi_data->dims[1]);
-        }
-
-        int inv_y_coord =
-            mpi_layer.mpi_data->dims[0] - mpi_layer.mpi_data->coords[0] - 1;
-        y_offset = inv_y_coord * static_cast<int>(domain_height /
-                                                  mpi_layer.mpi_data->dims[0]);
-    }
-
-    void write_velocity_profile(
-        const LatticeBoltzmann::VelocityProfile::HostMirror &velocity_profile,
-        int iteration) {
-        output_file << "--- i:" << iteration << "\n";
-
-        double x_vel = 0, y_vel = 0;
-
-        for (int x = ghost_layers.left; x < lattice_width - ghost_layers.right;
-             x++) {
-            for (int y = ghost_layers.down;
-                 y < lattice_height - ghost_layers.up; y++) {
-                x_vel = velocity_profile(x, y, 0);
-                y_vel = velocity_profile(x, y, 1);
-
-                output_file << fmt::format(
-                    "{:d},{:d},{:f},{:f}\n", x + x_offset - ghost_layers.left,
-                    y + y_offset - ghost_layers.down, x_vel, y_vel);
-            }
-        }
-
-        output_file << std::flush;
-    }
-};
-
 void get_cmd_args(int argc, char *argv[], int &domain_width, int &domain_height,
-                  int &iterations, std::filesystem::path &output_path) {
+                  int &iterations) {
     for (int i = 1; i < argc; i++) {
         if (strncmp(argv[i], "--width", 7) == 0) {
             domain_width = atoi(argv[++i]);
@@ -88,20 +26,13 @@ void get_cmd_args(int argc, char *argv[], int &domain_width, int &domain_height,
         if (strncmp(argv[i], "--iterations", 12) == 0) {
             iterations = atoi(argv[++i]);
         }
-
-        if (strncmp(argv[i], "--output-path", 13) == 0) {
-            output_path = std::filesystem::canonical(argv[++i]);
-        }
     }
 }
 
 int main(int argc, char *argv[]) {
     int domain_width = 0, domain_height = 0, iterations = 0;
 
-    std::filesystem::path output_directory = std::filesystem::canonical("./");
-
-    get_cmd_args(argc, argv, domain_width, domain_height, iterations,
-                 output_directory);
+    get_cmd_args(argc, argv, domain_width, domain_height, iterations);
 
     if (domain_width == 0 || domain_height == 0 || iterations == 0) {
         std::cout << "Please provide the --width and --height of the domain "
@@ -116,9 +47,6 @@ int main(int argc, char *argv[]) {
 
     {
         MPILayer mpi_layer(domain_width, domain_height);
-        LatticeBoltzmann::VelocityProfile final_velocity_profile;
-        DistributedVelocityProfileOutput velocity_profile_output(
-            output_directory, domain_width, domain_height, mpi_layer);
 
         {
             std::cout << fmt::format("{:d}: Using GPU with Device ID {:d}",
@@ -213,17 +141,7 @@ int main(int argc, char *argv[]) {
                                      mpi_layer.mpi_data->rank, domain_width,
                                      domain_height, iterations, elapsed, mlups)
                       << std::endl;
-
-            final_velocity_profile = std::move(velocity_profile);
         }
-
-        LatticeBoltzmann::VelocityProfile::HostMirror final_velocity_mirror =
-            Kokkos::create_mirror_view(final_velocity_profile);
-
-        Kokkos::deep_copy(final_velocity_mirror, final_velocity_profile);
-
-        velocity_profile_output.write_velocity_profile(final_velocity_mirror,
-                                                       0);
     }
 
     Kokkos::finalize();
